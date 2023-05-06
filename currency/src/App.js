58fb1737915b8fdcd83a54ed1295c49957466931
Davey Melero <Davey1234576@gmail.com>
2023-05-05 22:59:39 -0700

    added a Components folder and a routes folder with files inside

--- currency/src/App.js
+++ currency/src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
-import  { ThemeProvider } from './context/ThemeContext'
+import React from "react"
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
+
 
 function App() {
   return <ThemeProvider>
-
+      <Navbar />
   </ThemeProvider>
+
   
   
 }
