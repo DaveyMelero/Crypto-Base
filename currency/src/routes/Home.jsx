e92cf0bc7cc753f772ef14748041008f5c9c3e8c
Davey Melero <Davey1234576@gmail.com>
2023-05-10 15:36:01 -0700

    added a new file Trending.jsx and add Trending in Home.jsx file

--- currency/src/routes/Home.jsx
+++ currency/src/routes/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import CoinSearch from '../components/CoinSearch'
+import Trending from '../components/Trending'
 
 const Home = ({coins}) => {
   return (
     <div>
       <CoinSearch coins={coins} />
+      <Trending />
+      
     </div>
+  
   )
 }
 
 export default Home
+
